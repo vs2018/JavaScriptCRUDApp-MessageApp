1b10e3227efccfacb865a38c62cd1cfb5f19d84d
Vishal Sharma <vsh1819@gmail.com>
2018-04-10 10:21:12 +0100

    created changeformestate function for edit where we change the state of the form

--- src/ui.js
+++ src/ui.js
@@ -57,6 +57,7 @@ class UI {
     }, 3000)
   }
 
+  // Clear alert message
   clearAlert(){
     const currentAlert = document.querySelector('.alert')
 
@@ -69,6 +70,53 @@ class UI {
     this.titleInput.value = ''
     this.bodyInput.value = ''
   }
+
+  // Film form to edit
+  fillForm(data){
+    this.titleInput.value = data.title
+    this.bodyInput.value = data.body
+    this.idInput.value = data.id
+
+    this.changeFormState('edit')
+  }
+
+  clearIdInput(){
+    this.idInput.value = ''
+  }
+
+  // Change the form state
+  changeFormState(type){
+    if(type === 'edit'){
+      this.postSubmit.textContent = 'Update Post'
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block'
+
+      // Create cancel button
+      const button = document.createElement('button')
+      button.className = 'post-cancel btn btn-light btn-block'
+      button.appendChild(document.createTextNode('Cancel Edit'))
+
+      // Get parent
+      const cardForm = document.querySelector('.card-form')
+      // Get element to insert before
+      const formEnd = document.querySelector('.form-end')
+      // Insert cancel button
+      cardForm.insertBefore(button, formEnd)
+    } else {
+      this.postSubmit.textContent = 'Post It'
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block'
+      // remove cancel button if it is there
+      if(document.querySelector('.post-cancel')){
+        document.querySelector('.post-cancel').remove()
+
+      }
+
+      // Clear ID from hidden field
+      this.clearIdInput()
+      // Clear text
+      this.clearFields()
+    }
+  }
+
 }
 
 export const ui = new UI()
