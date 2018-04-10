2e3ffd89b5b5d3fcb3d4b400aed0777bc7788080
Vishal Sharma <vsh1819@gmail.com>
2018-04-10 10:02:40 +0100

    created eventlistener for editing a post and created the enableedit function in app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts)
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost)
 
+// List for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit)
+
 function getPosts(){
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
@@ -30,4 +33,28 @@ function submitPost(){
     getPosts()
   })
   .catch(err => console.log(err))
+}
+
+// Enable Edit State
+function enableEdit(e){
+  if(e.target.parentElement.classList.contains('edit')){
+    const id = e.target.parentElement.dataset.id
+
+    const body = e.target.parentElement.previousElementSibling.textContent
+
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Film form with current post
+    ui.fillForm(data)
+
+  }
+  
+
+  e.preventDefault()
 }
