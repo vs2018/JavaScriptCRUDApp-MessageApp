332f305a85f4c1ec6cd627b3f52ad70c9335bb24
Vishal Sharma <vsh1819@gmail.com>
2018-04-10 09:49:17 +0100

    created a show alert function to show that a post has been added

--- src/app.js
+++ src/app.js
@@ -25,6 +25,8 @@ function submitPost(){
   //Create Post
   http.post('http://localhost:3000/posts', data)
   .then(data => {
+    ui.showAlert('Post added', 'alert alert-success')
+    ui.clearFields()
     getPosts()
   })
   .catch(err => console.log(err))
